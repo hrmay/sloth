616812467d0a7a319e551378eb519d8f02042dae
Evan May <jmay3@mail.umw.edu>
2017-02-21 04:34:50 +0000

    working lexer

---
+++ tokens.h
@@ -0,0 +1,43 @@
+#ifndef TOKENS_SL
+#define TOKENS_S
+
+#define IDENT   100
+#define VALUE   101
+
+// Arithmetic Operators
+#define PLUS    102
+#define MINUS   103
+#define DIVIDE  104
+#define TIMES   105
+
+// Comparison Operators
+#define LTHAN   106
+#define GTHAN   107
+#define LEQ     108
+#define GEQ     109
+#define EQUALS  110
+#define NEQ     111
+
+// Logical Operators
+#define AND     112
+#define OR      113
+#define NOT     114
+
+// Other Symbols
+#define SCOL    115
+#define SET     116
+#define OPAREN  117
+#define CPAREN  118
+
+// Reserved Words
+#define START   119
+#define END     120
+#define IF      121
+#define THEN    122
+#define ELSE    123
+#define WHILE   124
+#define DO      125
+#define PRINT   126
+#define INPUT   127
+
+#endif
